dc72a0befafa4599332d45b31559d3b7cd47d5d3
Konstantin Kurganov <kurganovk@gmail.com>
2021-03-31 16:04:45 +0300

    wip: record controls

--- src/pages/video-editor/EditorBar.tsx
+++ src/pages/video-editor/EditorBar.tsx
@@ -6,12 +6,21 @@ import {
   Paper,
   Theme,
 } from '@material-ui/core';
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import StopIcon from '@material-ui/icons/Stop';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import DeleteIcon from '@material-ui/icons/Delete';
+// @ts-ignore
+import MicRecorder from 'mic-recorder-to-mp3';
+import { AixmusicApi } from '../../lib/aixmusic-api/AixmusicApi';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/rootReducer';
+
+const Mp3Recorder = new MicRecorder({ bitRate: 128 });
+const api = AixmusicApi.getInstance();
 
 interface Props {}
 
@@ -32,9 +41,60 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-export default function EditorBar({}: Props): ReactElement {
+export default function EditorBar(props: Props): ReactElement {
+  const state = useSelector((state: RootState) => state.presentation);
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
+
+  const [isRecording, setIsRecording] = useState(false);
+  const [isBlocked, setIsBlocked] = useState(false);
+
+  useEffect(() => {
+    navigator.getUserMedia(
+      { audio: true },
+      () => {
+        console.log('Permission Granted');
+        setIsBlocked(false);
+      },
+      () => {
+        console.log('Permission Denied');
+        setIsBlocked(true);
+      }
+    );
+  }, []);
+
+  const startRec = () => {
+    if (isBlocked) {
+      console.log('Permission Denied');
+    } else {
+      Mp3Recorder.start()
+        .then(() => {
+          setIsRecording(true);
+        })
+        .catch((e: any) => console.error(e));
+    }
+  };
+
+  const stopRec = () => {
+    Mp3Recorder.stop()
+      .getMp3()
+      // @ts-ignore
+      .then(async ([buffer, blob]) => {
+        await api.updateSlideAudio(state.selectedSlide.id, blob);
+        setIsRecording(false);
+      })
+      .catch((e: any) => console.log(e));
+  };
+
+  const recIcon = isRecording ? <StopIcon /> : <FiberManualRecordIcon />;
+  const recLabel = isRecording ? "Stop" : "Record";
+  const onRecClick = () => {
+    if (isRecording) 
+      stopRec(); 
+    else 
+      startRec();
+  }
+
   return (
     <Paper variant="outlined" className={classes.root}>
       <BottomNavigation
@@ -45,9 +105,19 @@ export default function EditorBar({}: Props): ReactElement {
         }}
       
       >
-        <BottomNavigationAction label="Record" icon={<FiberManualRecordIcon />} />
-        <BottomNavigationAction label="Play" icon={<PlayArrowIcon />} />
-        <BottomNavigationAction label="Delete" icon={<DeleteIcon />} />
+        <BottomNavigationAction 
+          label={recLabel} 
+          icon={recIcon} 
+          onClick={onRecClick}
+        />
+        <BottomNavigationAction 
+          label="Play" 
+          icon={<PlayArrowIcon />} 
+        />
+        <BottomNavigationAction 
+          label="Delete" 
+          icon={<DeleteIcon />} 
+        />
       </BottomNavigation>
     </Paper>
   );
--- src/pages/video-editor/VideoEditorPage.tsx
+++ src/pages/video-editor/VideoEditorPage.tsx
@@ -16,15 +16,11 @@ import { useAppDispatch } from '../../redux/store';
 import Paper from '@material-ui/core/Paper';
 import { useHistory, useParams } from 'react-router-dom';
 import { SlidesView } from './SlidesView';
-// @ts-ignore
-import MicRecorder from 'mic-recorder-to-mp3';
-import { AixmusicApi } from '../../lib/aixmusic-api/AixmusicApi';
 import EditorBar from './EditorBar';
 import { SlidesViewBottomRow } from './SlidesViewBottomRow';
 
 const uploadsUrl = process.env.REACT_APP_UPLOADS_URL as string;
-const Mp3Recorder = new MicRecorder({ bitRate: 128 });
-const api = AixmusicApi.getInstance();
+
 
 interface Props {}
 
@@ -57,53 +53,12 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export const VideoEditorPage = (props: Props) => {
   const url = 'test';
-  // const state = useSelector((state: RootState) => state.presentation);
   const dispatch = useAppDispatch();
   const history = useHistory();
   const classes = useStyles();
   const state = useSelector((state: RootState) => state.presentation);
 
   let { presentationUrl } = useParams<ParamTypes>();
-
-  const [isRecording, setIsRecording] = useState(false);
-  const [isBlocked, setIsBlocked] = useState(false);
-
-  useEffect(() => {
-    navigator.getUserMedia(
-      { audio: true },
-      () => {
-        console.log('Permission Granted');
-        setIsBlocked(false);
-      },
-      () => {
-        console.log('Permission Denied');
-        setIsBlocked(true);
-      }
-    );
-  }, []);
-
-  const startRec = () => {
-    if (isBlocked) {
-      console.log('Permission Denied');
-    } else {
-      Mp3Recorder.start()
-        .then(() => {
-          setIsRecording(true);
-        })
-        .catch((e: any) => console.error(e));
-    }
-  };
-
-  const stopRec = () => {
-    Mp3Recorder.stop()
-      .getMp3()
-      // @ts-ignore
-      .then(async ([buffer, blob]) => {
-        await api.updateSlideAudio(state.selectedSlide.id, blob);
-        setIsRecording(false);
-      })
-      .catch((e: any) => console.log(e));
-  };
 
   return (
     <div className={classes.root}>
@@ -126,7 +81,7 @@ export const VideoEditorPage = (props: Props) => {
           }}>
             test
           </button> */}
-          <br></br>
+          {/* <br></br>
           <button onClick={startRec} disabled={isRecording}>
             Record
           </button>
@@ -137,7 +92,7 @@ export const VideoEditorPage = (props: Props) => {
             src={`${uploadsUrl}${state.selectedSlide.audio}`}
             controls={true}
           />
-          <br></br>
+          <br></br> */}
           <img src={`${uploadsUrl}${state.selectedSlide.image}`} />
           <br></br>
           <EditorBar />
