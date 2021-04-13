24744c96431a5120d8bc6d6b89168b842ddd71e2
Konstantin Kurganov <kurganovk@gmail.com>
2021-04-13 10:30:45 +0300

    Fix styles

--- src/pages/video-editor/SlidesView.tsx
+++ src/pages/video-editor/SlidesView.tsx
@@ -8,17 +8,29 @@ import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import PdfReader from '../../lib/pdf/PdfReader';
-import { getPresentation, updateSlideOrder } from '../../redux/presentation/presentationThunks';
+import {
+  getPresentation,
+  updateSlideOrder,
+} from '../../redux/presentation/presentationThunks';
 import { RootState } from '../../redux/rootReducer';
 import { useAppDispatch } from '../../redux/store';
 import { SlidePreview } from './SlidePreview';
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import { setPresentationSlides } from '../../redux/presentation/presentationSlice';
 import { ISlideResponse } from '../../types/AixmusicApiTypes';
 
 interface Props {
   presentationUrl: string;
 }
+
+const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
+  userSelect: 'none',
+  ...draggableStyle,
+});
+
+const getListStyle = (isDraggingOver: boolean) => ({
+  width: '100%',
+});
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -44,42 +56,23 @@ export const SlidesView = (props: Props) => {
     dispatch(getPresentation(props.presentationUrl));
   }, [history]);
 
-  const reorder = (list: ISlideResponse[], startIndex: number, endIndex: number): ISlideResponse[] => {
+  const reorder = (
+    list: ISlideResponse[],
+    startIndex: number,
+    endIndex: number
+  ): ISlideResponse[] => {
     const result = [...list];
     const [removed] = result.splice(startIndex, 1);
     result.splice(endIndex, 0, removed);
     return result.map((item, index) => {
-      const retItem = {...item, order: index};
+      const retItem = { ...item, order: index };
       // TODO: maybe freeze
-      dispatch(updateSlideOrder({slideID: retItem.id, order: retItem.order}));
+      dispatch(updateSlideOrder({ slideID: retItem.id, order: retItem.order }));
       return retItem;
     });
   };
 
-  const grid = 8;
-
-  // @ts-ignore
-  const getItemStyle = (isDragging: boolean, draggableStyle) => ({
-    // some basic styles to make the items look a bit nicer
-    userSelect: "none",
-    padding: grid * 2,
-    margin: `0 0 ${grid}px 0`,
-
-    // change background colour if dragging
-    background: isDragging ? "lightgreen" : "grey",
-
-    // styles we need to apply on draggables
-    ...draggableStyle
-  });
-
-  const getListStyle = (isDraggingOver: boolean) => ({
-    background: isDraggingOver ? "lightblue" : "lightgrey",
-    padding: grid,
-    width: 250
-  });
-
-  // @ts-ignore
-  const onDragEnd = (result) => {
+  const onDragEnd = (result: any) => {
     // dropped outside the list
     if (!result.destination) {
       return;
@@ -92,7 +85,7 @@ export const SlidesView = (props: Props) => {
     );
 
     dispatch(setPresentationSlides(items));
-  }
+  };
 
   return (
     <div className={classes.root}>
@@ -100,35 +93,43 @@ export const SlidesView = (props: Props) => {
         <CircularProgress color="secondary" />
       ) : (
         <DragDropContext onDragEnd={onDragEnd}>
-          <Droppable droppableId="droppable">            
-            {// @ts-ignore
-            (provided, snapshot) => (
-              <div
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-                style={getListStyle(snapshot.isDraggingOver)}
-              >
-                {state.presentation.slides?.map((item, index) => (
-                  <Draggable key={item.id} draggableId={item.id.toString()} index={index}>
-                    {// @ts-ignore
-                    (provided, snapshot) => (
-                      <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        style={getItemStyle(
-                          snapshot.isDragging,
-                          provided.draggableProps.style
-                        )}
-                      >
-                        <SlidePreview slide={item} key={item.id} />
-                      </div>
-                    )}
-                  </Draggable>
-                 ))}
-                {provided.placeholder}
-              </div>
-            )}
+          <Droppable droppableId="droppable">
+            {
+              // @ts-ignore
+              (provided, snapshot) => (
+                <div
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  style={getListStyle(snapshot.isDraggingOver)}
+                >
+                  {state.presentation.slides?.map((item, index) => (
+                    <Draggable
+                      key={item.id}
+                      draggableId={item.id.toString()}
+                      index={index}
+                    >
+                      {
+                        // @ts-ignore
+                        (provided, snapshot) => (
+                          <div
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            style={getItemStyle(
+                              snapshot.isDragging,
+                              provided.draggableProps.style
+                            )}
+                          >
+                            <SlidePreview slide={item} key={item.id} />
+                          </div>
+                        )
+                      }
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )
+            }
           </Droppable>
         </DragDropContext>
       )}
